79cefdb4f103ccba4a63cc3f7876fede0a38f5f4
Sain-orshikh <162454448+Sain-orshikh@users.noreply.github.com>
2026-01-18 02:44:00 +0800

    fix: update favicon link to use ICO format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang='en' suppressHydrationWarning>
       <head>
-        <link rel='icon' href='/favicon.svg' type='image/svg+xml' />
+        <link rel='icon' href='/favicon.ico' type='image/x-icon' />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#5d87ff" />
       </head>
