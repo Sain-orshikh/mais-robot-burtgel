9511e06068bbcee18da3b636a4057688af817827
Sain-orshikh <162454448+Sain-orshikh@users.noreply.github.com>
2026-01-18 03:30:12 +0800

    fix: update CORS options to allow all routes for preflight requests

--- backend/server.js
+++ backend/server.js
@@ -56,7 +56,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options('/*', cors(corsOptions));
 
 app.use(express.json({limit: "5mb"})); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse form data
